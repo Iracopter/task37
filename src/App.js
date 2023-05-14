452e50c799eabaa70ccc8460fda378958ddf12ad
Iracopter <114257500+Iracopter@users.noreply.github.com>
2023-05-14 22:51:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { My_Photo } from './components/My_Photo';
 import {Contact_Info} from './components/Contact_Info';
-import {Work_Experience} from './components/Work_Experience'
+import {Work_Experience} from './components/Work_Experience';
+import photo from './components/0000.jpg';
+
+const imgLink=photo;
 
 function App() {
   return (
     <div className='wrapper'>
-        <My_Photo path="src/0000.jpg"/>
+        <My_Photo path={imgLink}/>
         <Contact_Info/>
         <Work_Experience/>
     </div>
